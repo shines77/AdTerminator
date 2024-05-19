f8e72e2b5acf92d2fd7321a0969464227feefbf6
shines77 <gz_shines@msn.com>
2024-05-19 16:58:28 +0800

    Added utf8_substr()
    Signed-off-by: shines77 <gz_shines@msn.com>

--- Core/Utils.lua
+++ Core/Utils.lua
@@ -43,3 +43,69 @@ end
 function addonNS.UnitName(unit)
     return addonNS.GetFullName(UnitFullName(unit))
 end
+
+--
+-- From: https://wowprogramming.com/snippets/UTF-8_aware_stringsub_7.html
+--
+
+-- UTF-8 Reference:
+-- 0xxxxxxx - 1 byte UTF-8 codepoint (ASCII character)
+-- 110yyyxx - First byte of a 2 byte UTF-8 codepoint
+-- 1110yyyy - First byte of a 3 byte UTF-8 codepoint
+-- 11110zzz - First byte of a 4 byte UTF-8 codepoint
+-- 10xxxxxx - Inner byte of a multi-byte UTF-8 codepoint
+
+local function utf8_size_slow(char)
+    if not char then
+        return 0
+    elseif char > 240 then
+        return 4
+    elseif char > 225 then
+        return 3
+    elseif char > 192 then
+        return 2
+    else
+        return 1
+    end
+end
+
+local function utf8_size(char)
+    if char >= 224 then
+        if char < 240 then
+            return 3
+        else
+            return 4
+        end
+    elseif char < 192 then
+        if char > 0 then
+            return 1
+        else
+            return 0
+        end
+    else
+        return 2
+    end
+end
+
+-- This function can return a substring of a UTF-8 string, properly handling
+-- UTF-8 codepoints.  Rather than taking a start index and optionally an end
+-- index, it takes the string, the starting character, and the number of
+-- characters to select from the string.
+
+local function utf8_substr(str, startChar, numChars)
+    local startIndex = 1
+    while startChar > 1 do
+        local char = string.byte(str, startIndex)
+        startIndex = startIndex + utf8_size(char)
+        startChar = startChar - 1
+    end
+
+    local totalChars = #str
+    local currentIndex = startIndex
+    while numChars > 0 and currentIndex <= totalChars do
+        local char = string.byte(str, currentIndex)
+        currentIndex = currentIndex + utf8_size(char)
+        numChars = numChars - 1
+    end
+    return str:sub(startIndex, currentIndex - 1)
+end
